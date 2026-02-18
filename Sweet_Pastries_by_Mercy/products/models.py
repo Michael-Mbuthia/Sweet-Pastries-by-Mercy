fa889b82cc002b7daa1eef81ea3cb9753ae8b65b
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2026-02-18 11:51:06 +0300

    Implement product model, serializers, and CRUD views

--- Sweet_Pastries_by_Mercy/products/models.py
+++ Sweet_Pastries_by_Mercy/products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ("cake", "Cake"),
+        ("bread", "Bread"),
+        ("pastry", "Pastry"),
+        ("cookie", "Cookie"),
+    ]
+
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock_quantity = models.PositiveIntegerField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
