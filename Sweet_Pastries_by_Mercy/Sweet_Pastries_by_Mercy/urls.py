3015f189946c2dfda4c4e2c30631750f3a972cf7
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2026-02-19 23:07:22 +0300

    Fix product URL typo and implement Cart and CartItem models with migrations

--- Sweet_Pastries_by_Mercy/Sweet_Pastries_by_Mercy/urls.py
+++ Sweet_Pastries_by_Mercy/Sweet_Pastries_by_Mercy/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/", include("accounts.urls")),
-    path("api/prroducts/", include("products.urls")),
+    path("api/products/", include("products.urls")),
 ]
--- Sweet_Pastries_by_Mercy/cart/models.py
+++ Sweet_Pastries_by_Mercy/cart/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.user.username}'s cart"  
+
+s
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.product.name} ({self.quantity})"
